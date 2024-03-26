47665567631c4947b263a3450bc4e53167674967
chenjunlin <1739560479@qq.com>
2024-03-26 21:50:31 +0800

    init

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'dumi';
 
-const repo = 'Fijiblueblog';
+const repo = 'myblog';
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
@@ -9,7 +9,7 @@ export default defineConfig({
     name: 'Canyon',
     // logo: '/avater.svg',
     socialLinks: {
-      github: 'https://github.com/Canyonmnmn',
+      github: 'https://github.com/Chen990925',
     },
     hideHomeNav: false,
   },
